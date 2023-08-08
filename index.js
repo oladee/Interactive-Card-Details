1d3d3e6b9f87d8173a9e7077d0eebbbe26440fb5
oladee <deemajor230600@gmail.com>
2023-08-08 23:14:42 +0100

    Almost done with DOM functionality

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ let cardMonth = document.getElementById('cardMonth');
 let cardYear = document.getElementById('cardYear');
 let cardCvv = document.getElementById('cardCvv');
 let form = document.getElementById('form');
-let varArray = ['Name','Month','Year','Number','Cvv']
-for(var i = 0; i < varArray.length; i++){
-    
-}
+names.addEventListener('input', () => {
+    cardName.innerText = names.value
+})
+year.addEventListener('input', () => {
+    cardYear.innerText = year.value
+})
+month.addEventListener('input', () => {
+    cardMonth.innerText = month.value
+})
+number.addEventListener('input', () => {
+    let numArr = number.value.split('')
+    for(var i = 0; numArr.length < 14; i++){
+            numArr.push('0')
+    }
+    cardNumber.innerText = numArr.join('')
+})
+cvv.addEventListener('input', () => {
+    cardCvv.innerText = cvv.value
+})
