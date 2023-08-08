245e3b21e30c81425a561bc276f900a3f3d128c3
oladee <deemajor230600@gmail.com>
2023-08-08 16:01:23 +0100

    working out card functionalities

---
+++ index.js
@@ -0,0 +1,15 @@
+let names = document.getElementById('names');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let number = document.getElementById('number');
+let cvv = document.getElementById('cvv');
+let cardNumber = document.getElementById('cardNumber');
+let cardName = document.getElementById('cardName');
+let cardMonth = document.getElementById('cardMonth');
+let cardYear = document.getElementById('cardYear');
+let cardCvv = document.getElementById('cardCvv');
+let form = document.getElementById('form');
+let varArray = ['Name','Month','Year','Number','Cvv']
+for(var i = 0; i < varArray.length; i++){
+    
+}
