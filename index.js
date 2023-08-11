2ca0f6deaf8b42295e15afe5f94c163575ca2c8e
oladee <deemajor230600@gmail.com>
2023-08-11 11:31:45 +0100

    name validation

--- index.js
+++ index.js
@@ -9,18 +9,41 @@ let cardMonth = document.getElementById('cardMonth');
 let cardYear = document.getElementById('cardYear');
 let cardCvv = document.getElementById('cardCvv');
 let form = document.getElementById('form');
-names.addEventListener('input', () => {
-    cardName.innerText = names.value
-})
+const nameValidation = () => {
+    let Names = names.value
+    let validName = Names.match(/[0-9]/g)
+    if(validName){
+        let error = "Please Enter A Valid Name"
+        console.log(error)
+    }else{
+        error = null
+    }
+    if(names.value.length < 1){
+        cardName.innerText = 'Jane Appleseed'
+    }
+    else{
+        cardName.innerText = names.value
+    }
+}
+names.addEventListener('input', nameValidation)
 year.addEventListener('input', () => {
-    cardYear.innerText = year.value
+    let yearArr = year.value.split('')
+    for(var i = 0; yearArr.length < 2; i++){
+            yearArr.push('0')
+    }
+    let validatedNumber = yearArr.join('')
+    let valid = validatedNumber.match(/[a-z]/ig)
 })
 month.addEventListener('input', () => {
-    cardMonth.innerText = month.value
+    let monthArr = month.value.split('')
+    for(var i = 0; monthArr.length < 2; i++){
+            monthArr.push('0')
+    }
+    cardMonth.innerText = monthArr.join('')
 })
 number.addEventListener('input', () => {
     let numArr = number.value.split('')
-    for(var i = 0; numArr.length < 14; i++){
+    for(var i = 0; numArr.length < 16; i++){
             numArr.push('0')
     }
     cardNumber.innerText = numArr.join('')
